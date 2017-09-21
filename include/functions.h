3934b3b02a4b0d7af0f31a6b9a5c01374bc213d2
gaoyu <mathero@126.com>
2017-09-21 17:35:09 +0800

    easy hello

---
+++ include/functions.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: functions.h
+	> Author: gaoyu
+	> Mail: 1400012705@pku.edu.cn
+	> Created Time: 2017年09月21日 星期四 17时18分45秒
+ ************************************************************************/
+
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+
+class hello_printer {
+    virtual void doit();
+};
+
+#endif
---
+++ src/functions.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: ../src/functions.cpp
+	> Author: gaoyu
+	> Mail: 1400012705@pku.edu.cn
+	> Created Time: 2017年09月21日 星期四 17时22分22秒
+ ************************************************************************/
+
+#include <iostream>
+#include "functions.h"
+
+void hello_printer::doit() {
+    std::cout << "hello world" << std::endl;
+}
